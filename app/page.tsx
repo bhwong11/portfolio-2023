d0dee9f52002dd74aae980eef917c46442d84286
Bryant Wong <bryanthwong@gmail.com>
2023-11-18 18:06:26 -0500

    added about and section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
-import Header from './(components)/header'
+import Header from '@/app/(components)/Header'
+import About from '@/app/(components)/About'
+import SectionDivider from '@/app/(components)/SectionDivider'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header/>
+      <SectionDivider/>
+      <About/>
     </main>
   )
 }
