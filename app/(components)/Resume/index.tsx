aa12110aed083c9cd2c2dce4d145a4b454d31033
Bryant Wong <bryanthwong@gmail.com>
2023-11-24 00:01:55 -0500

    resume attempt

--- app/(components)/Resume/index.tsx
+++ app/(components)/Resume/index.tsx
@@ -20,8 +20,9 @@ export default function Resume(){
           <SectionHeading>Want To Work Togther?</SectionHeading>
           <a
             className="font-mono px-4 w-max flex items-center justify-center h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 disabled:scale-100 disabled:opacity-50"
-            href="/resume.pdf"
-            download
+            href="/_next/resume.pdf"
+            target="_blank"
+            rel="noreferrer"
           >
             View Resume
           </a>
